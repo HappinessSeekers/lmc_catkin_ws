8e7d879cc45d0786b81e216fa22c71c2527c2cbc
mingcong <mingcong.li@plus.ai>
2020-03-23 06:53:57 -0700

    import MRAC controller framework

---
+++ src/testbench_interface/include/testbench_interface/mrac_contorller.h
@@ -0,0 +1,11 @@
+#ifndef MRAC_CONTROLLER_H
+#define MRAC_CONTROLLER_H
+
+class MRAC_Algorithm
+{
+
+};
+
+
+
+#endif // MRAC_CONTROLLER_H
---
+++ src/testbench_interface/src/mrac_controller.cpp
@@ -0,0 +1,7 @@
+#include "testbench_interface/mainwindow.h"
+
+PID_Algorithm::MRAC_Algorithm(const float& controller_frequency)
+{
+
+}
+
